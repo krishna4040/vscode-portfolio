27c4f0e62644cbe6655c586632196044b04b2387
krishna jain <krishnajain5050@gmail.com>
2025-01-15 17:53:28 +0530

    about section updated

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,14 +1,27 @@
+import Experience from "../components/Experience";
+
 const AboutPage = () => {
   return (
     <>
-      <h3>A Little Bit About Me</h3>
+      <div>
+        <h1 style={{ textAlign: "center" }}>About Me 🌟</h1>
+        <p style={{ textAlign: "center", margin: "0 auto", maxWidth: "650px" }}>
+          👨‍💻 Full Stack Developer | ⚡ Stack Independent (Specializing
+          JavaScript)
+          <br />
+          🌐 RESTful Services | ☁️ Cloud Engineer (AWS, Docker, Kubernetes)
+          <br />
+          🎓 B.Tech in IT from Punjab University (2026, CGPA: 8.3)
+        </p>
+        <Experience />
+      </div>
     </>
   );
 };
 
 export async function getStaticProps() {
   return {
-    props: { title: 'About' },
+    props: { title: "About" },
   };
 }
 
